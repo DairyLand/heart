906e8fdfdb45209b3573b17cc57f8f26b8336c73
Hunter <farmerhunter@proton.me>
2023-02-01 12:05:09 -0500

    added firebase.js

---
+++ firebase.js
@@ -0,0 +1,15 @@
+import * as firebase from 'firebase';
+
+const firebaseConfig = {
+  apiKey: "your-api-key",
+  authDomain: "your-auth-domain",
+  databaseURL: "your-database-url",
+  projectId: "your-project-id",
+  storageBucket: "your-storage-bucket",
+  messagingSenderId: "your-sender-id",
+  appId: "your-app-id"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export default firebase;
