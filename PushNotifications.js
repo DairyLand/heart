92d2d45af8cdb3526e0a531fd5293a76c91d8509
Hunter <farmerhunter@proton.me>
2023-02-02 20:42:24 -0500

    fixed a error in the pushnotifications

--- PushNotifications.js
+++ PushNotifications.js
@@ -2,11 +2,11 @@ import * as firebase from 'firebase';
 
 export const sendPushNotification = async () => {
   try {
-    const response = await firebase
+    const fcmTokenResponse = await firebase
       .database()
       .ref(`/linked_user_1/fcmToken`)
       .once('value');
-    const fcmToken = response.val();
+    const fcmToken = fcmTokenResponse.val();
     // Send push notification to linked user here
     const message = {
       to: fcmToken,
@@ -15,8 +15,8 @@ export const sendPushNotification = async () => {
         body: `User 1 (username) needs attention asap!`,
       },
     };
-    const response = await firebase.messaging().send(message);
-    console.log(response);
+    const sendResponse = await firebase.messaging().send(message);
+    console.log(sendResponse);
   } catch (error) {
     console.error(error);
   }
